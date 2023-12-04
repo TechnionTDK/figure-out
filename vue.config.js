853b051e1c50ff3aec53fa865156c99bd5eefafc
Oren Mishali <oren.mishali@gmail.com>
2023-12-04 14:41:24 +0200

    back to 8080 for dev server

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,4 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   lintOnSave: false,
   transpileDependencies: ["vuetify"],
-  devServer: {
-    port: 80
-  }
 });
